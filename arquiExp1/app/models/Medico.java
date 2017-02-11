824cfa498812e2667de26a3b2d0ae9f50faef5bf
Maria Camila Hernandez <mc.hernandez1@uniandes.edu.co>
2017-02-11 16:55:25 -0500

    Edición clase medico

--- arquiExp1/app/models/Medico.java
+++ arquiExp1/app/models/Medico.java
@@ -1,7 +1,39 @@
 package models;
 
+import com.avaje.ebean.Model;
+import com.fasterxml.jackson.databind.JsonNode;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+
 /**
+ * Clase que representa la información de un medico
  * Created by af.moreno10 on 10/02/2017.
  */
-public class Medico {
+
+//@Entity
+
+public class Medico extends Model
+{
+     //-----------------------------------------------------------
+    // Atributos
+    //-----------------------------------------------------------
+
+    /**
+     * Referencia que identifica al medico en el sistema.
+     */
+    //@Id
+    //@GeneratedValue(strategy=GenerationType.IDENTITY)
+    //private Long referencia;
+
+    /**
+     * Nombre del medico.
+     */
+    private String nombreMedico;
+
+    /**
+     * Descripción del medico.
+     */
+    private String descripcionMedico;
 }
