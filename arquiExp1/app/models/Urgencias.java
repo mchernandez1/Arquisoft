236569daf71dacd700fa61ebd7c8a8813e7b97bf
JmLizarazo10 <jm.lizarazo10@uniandes.edu.co>
2017-02-12 13:33:06 -0500

    cambios

--- arquiExp1/app/models/Urgencias.java
+++ arquiExp1/app/models/Urgencias.java
@@ -9,4 +9,33 @@ public class Urgencias {
     private Long longitud;
     private String estadoPaciente;
 
+    public Urgencias(Long latitud, Long longitud, String estadoPaciente){
+        this.latitud = latitud;
+        this.longitud = longitud;
+        this.estadoPaciente = estadoPaciente;
+    }
+
+    public Long getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(Long latitud) {
+        this.latitud = latitud;
+    }
+
+    public Long getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(Long longitud) {
+        this.longitud = longitud;
+    }
+
+    public String getEstadoPaciente() {
+        return estadoPaciente;
+    }
+
+    public void setEstadoPaciente(String estadoPaciente) {
+        this.estadoPaciente = estadoPaciente;
+    }
 }
